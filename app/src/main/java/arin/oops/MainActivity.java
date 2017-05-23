a4f0f89e5ba984ce2b0ada454d4361a6abbf3c6d
runninsavvy <runninsavvy@gmail.com>
2017-05-23 18:43:59 -0400

    Added Easter Egg

--- app/src/main/java/arin/oops/MainActivity.java
+++ app/src/main/java/arin/oops/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -13,9 +15,12 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer oopsplayer;
     private MediaPlayer preplayer;
     private MediaPlayer fatplayer;
+    private MediaPlayer jonplayer;
+    ImageView jon;
     Button oopsButton;
     Button preButton;
     Button fatButton;
+    ImageButton arinButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +28,27 @@ public class MainActivity extends AppCompatActivity {
         oopsplayer = MediaPlayer.create(MainActivity.this, R.raw.oops);
         preplayer = MediaPlayer.create(MainActivity.this, R.raw.pre);
         fatplayer = MediaPlayer.create(MainActivity.this, R.raw.fat);
+        jonplayer = MediaPlayer.create(MainActivity.this, R.raw.jon);
+        jon = (ImageView) findViewById(R.id.imageView);
         addListenerOnButton();
+        /*Thread jonDetector = new Thread() {
+            public void run() {
+                if(jonplayer.isPlaying()) {
+                    jon.setVisibility(View.VISIBLE);
+                }
+                else {
+                    jon.setVisibility(View.INVISIBLE);
+                }
+            }
+        };
+        jonDetector.start();*/
     }
 
     public void addListenerOnButton() {
         oopsButton = (Button) findViewById(R.id.button6);
         preButton = (Button) findViewById(R.id.button8);
         fatButton = (Button) findViewById(R.id.button7);
+        arinButton = (ImageButton) findViewById(R.id.imageButton2);
         oopsButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 oopsplayer.start();
             }
         });
+
         preButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 preplayer.start();
             }
         });
+
         fatButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +76,17 @@ public class MainActivity extends AppCompatActivity {
                     fatplayer.seekTo(0);
                 }
                 fatplayer.start();
+            }
+        });
+
+        arinButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jon.setVisibility(View.VISIBLE);
+                if (jonplayer.isPlaying()) {
+                    jonplayer.seekTo(0);
+                }
+                jonplayer.start();
             }
         });
     }
