1acb2ff7f1d0384c2bdeed9c84c80f1a79540463
runninsavvy <runninsavvy@gmail.com>
2017-05-23 15:28:45 -0400

    Better button functionality

--- app/src/main/java/arin/oops/MainActivity.java
+++ app/src/main/java/arin/oops/MainActivity.java
@@ -3,13 +3,19 @@ package arin.oops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.widget.Button;
 import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer oopsplayer;
     private MediaPlayer preplayer;
     private MediaPlayer fatplayer;
+    Button oopsButton;
+    Button preButton;
+    Button fatButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,26 +23,39 @@ public class MainActivity extends AppCompatActivity {
         oopsplayer = MediaPlayer.create(MainActivity.this, R.raw.oops);
         preplayer = MediaPlayer.create(MainActivity.this, R.raw.pre);
         fatplayer = MediaPlayer.create(MainActivity.this, R.raw.fat);
+        addListenerOnButton();
     }
 
-    public void playoops(View view) {
-        if(oopsplayer.isPlaying()) {
-            oopsplayer.seekTo(0);
-        }
-        oopsplayer.start();
-    }
-
-    public void playpre(View view) {
-        if(preplayer.isPlaying()) {
-            preplayer.seekTo(0);
-        }
-        preplayer.start();
-    }
-
-    public void playfat(View view) {
-        if(fatplayer.isPlaying()) {
-            fatplayer.seekTo(0);
-        }
-        fatplayer.start();
+    public void addListenerOnButton() {
+        oopsButton = (Button) findViewById(R.id.button6);
+        preButton = (Button) findViewById(R.id.button8);
+        fatButton = (Button) findViewById(R.id.button7);
+        oopsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(oopsplayer.isPlaying()) {
+                    oopsplayer.seekTo(0);
+                }
+                oopsplayer.start();
+            }
+        });
+        preButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(preplayer.isPlaying()) {
+                    preplayer.seekTo(0);
+                }
+                preplayer.start();
+            }
+        });
+        fatButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fatplayer.isPlaying()) {
+                    fatplayer.seekTo(0);
+                }
+                fatplayer.start();
+            }
+        });
     }
 }
